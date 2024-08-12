e2ebd97a5de6e541fa1b0ac6bc4aac436f6e3fab
Andri <andrilol94@gmail.com>
2024-08-12 14:32:54 +0200

    Manca da fare la logica dei proiettil iche colpiscono i nemici, la morte dei nemici, il punteggio, le vite e la Vittoria e Sconfitta

--- src/SpaceShooterGame/Main.java
+++ src/SpaceShooterGame/Main.java
@@ -4,4 +4,5 @@ public class Main {
     public static void main(String[] args) {
         new MyFrame();
     }
+    //ao spacchi lo hai pure ottimizzato senza cercare nulla su internet e senza consultare ChatGPT, sapevi già come ottimizzarlo e cosa cambiare slay.
 }
